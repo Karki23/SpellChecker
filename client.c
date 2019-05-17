fa6b8027b5b690eb8c29afea2d39e7630e44a92e
Siddarth Karki <karkisiddarth@gmail.com>
2019-05-17 21:50:44 +0530

    added auto-suggest feature

--- client.c
+++ client.c
@@ -3,8 +3,13 @@
 #include "header.h"
 int main(int argc, char const *argv[])
 {
+
 	struct TrieNode *root = getNode();
 	frw("dict.txt",0,root); // inserts dictionary words into trie tree
 	frw("input.txt",1,root); //the main spell checeker part which outputs the line by line wrong words into a new output.txt
+	//char string[] = "";
+	//int level=0;
+
+	//display(root,string,level);
 	return 0;
 }
