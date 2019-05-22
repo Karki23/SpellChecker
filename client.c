560f1330e2b94f0aba689dcb3d2182b49fe9778f
Karki <karkisiddarth@gmail.com>
2019-05-22 20:46:02 +0530

    first commit in this branch

--- client.c
+++ client.c
@@ -6,6 +6,9 @@ int main(int argc, char const *argv[])
 
 	struct TrieNode *root = getNode();
 	frw("dict.txt",0,root); // inserts dictionary words into trie tree
-	frw("input.txt",1,root); //the main spell checeker part which outputs the line by line wrong words into a new output.txt
+	frw("input.txt",1,root); //the main spell checker part which outputs the line by line wrong words into a new output.txt
+	char str[100] = "";
+	int level =0;
+	display(root,str,level);
 	return 0;
 }
